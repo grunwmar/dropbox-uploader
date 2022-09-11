3d9f57885bfbf0f42568528b410cfe64bd16f978
Martin Grunwald <martingrunwald91@gmail.com>
2022-09-11 19:05:00 +0200

    comment in run.sh with example how to add prog to system

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 ./venv/bin/python3 . $@
+
+# export DB_ACCESS_TOKEN=$(cat $HOME'/.db_token')
+#
+# dbsend () {
+# 	cd /home/user/dropbox-uploader # example
+# 	bash run.sh $1
+# }
+#
+# dbsendpwd () {
+# 	PWDIR=$PWD
+# 	cd /home/user/dropbox-uploader # example
+# 	bash run.sh $PWDIR
+# }
